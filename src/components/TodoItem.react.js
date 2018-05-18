c523b126afcf0f3f2f6c700ae0f9467d69486a78
Pipat Waitayaworanart <coploftbas@gmail.com>
2018-05-18 14:35:09 +0700

    Statically checked checkbox
    - Read 'completed' from props and set defaultCheck of checkbox

--- src/components/TodoItem.react.js
+++ src/components/TodoItem.react.js
@@ -2,14 +2,15 @@ import React from 'react'
 
 class TodoItem extends React.Component {
     render() {
-
         const textStrikethrough = (checkCompleted) => {
             return checkCompleted ? { textDecoration: 'line-through' } : {}
         } 
-
+        
         return (
             <a className="panel-block" style={textStrikethrough(this.props.todoItemObj.completed)}>
-                <input type="checkbox" />
+                <input
+                    type="checkbox"
+                    defaultChecked={this.props.todoItemObj.completed} />
                 {this.props.todoItemObj.title}
             </a>
         )

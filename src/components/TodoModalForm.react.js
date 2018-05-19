72fb79b31a1d7b44a1b0a25beed48af68cde641a
Pipat Waitayaworanart <coploftbas@gmail.com>
2018-05-19 09:26:52 +0700

    Set state from input value
    - Retrieved value from input form. Then set state, but not yet assign to proper variable. Just log in console

--- src/components/TodoModalForm.react.js
+++ src/components/TodoModalForm.react.js
@@ -5,10 +5,16 @@ class TodoModalForm extends React.Component {
     state = {
         todoTitle: '',
         todoDesc: '',
-        
+        todoDate: '',
+        completed: false
+    }
+
+    addNewTodo() {
+        console.log(this.state)
     }
 
     render() {
+
         return (
             <div className="modal is-active">
                 <div className="modal-background"></div>
@@ -21,13 +27,22 @@ class TodoModalForm extends React.Component {
                         <div className="field">
                             <label className="label">TODO title</label>
                             <div className="control">
-                                <input className="input" type="text" placeholder="TODO title" />
+                                <input 
+                                    className="input" 
+                                    type="text" 
+                                    placeholder="TODO title" 
+                                    value={this.state.todoTitle} 
+                                    onChange={(event) => this.setState({ todoTitle: event.target.value })} />
                             </div>
                         </div>
                         <div className="field">
                             <label className="label">Description</label>
                             <div className="control">
-                                <textarea className="textarea" placeholder="TODO's description"></textarea>
+                                <textarea
+                                    className="textarea" 
+                                    placeholder="TODO's description" 
+                                    value={this.state.todoDesc} 
+                                    onChange={(event) => this.setState({ todoDesc: event.target.value })} />
                             </div>
                         </div>
                         <div className="field">
@@ -37,12 +52,13 @@ class TodoModalForm extends React.Component {
                                     id="datepicker"
                                     className="input"
                                     type="date"
-                                    onClick={() => console.log("Date input clicked")} />
+                                    onClick={() => console.log("Date input clicked")}
+                                    onChange={(event) => this.setState({ todoDate: event.target.value })} />
                             </div>
                         </div>
                     </section>
                     <footer className="modal-card-foot">
-                        <button className="button is-success">Save changes</button>
+                        <button className="button is-success" onClick={() => this.addNewTodo()}>Save changes</button>
                         <button className="button">Cancel</button>
                     </footer>
                 </div>
